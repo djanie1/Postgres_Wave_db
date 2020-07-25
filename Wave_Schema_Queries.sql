fc7c585ccacded1c6951122433e99d7331ad0cfe
djanie <kotey.seth11@gmail.com>
2020-07-25 12:59:44 +0000

    Update Wave_Schema_Queries .sql with Q6
    Update with Q6

--- Wave_Schema_Queries.sql
+++ Wave_Schema_Queries.sql
@@ -14,3 +14,11 @@ SELECT TO_CHAR(TO_DATE (EXTRACT(MONTH FROM when_created)::text, 'MM'), 'Month')	
 	COUNT(atx_id) FROM public.agent_transactions 
 	WHERE EXTRACT(YEAR FROM agent_transactions.when_created) = 2018 
 	GROUP BY EXTRACT(MONTH FROM agent_transactions.when_created);
+
+--Q6
+CREATE OR REPLACE VIEW atx_volume_city_summary AS
+SELECT COUNT(atx_id) AS volume, city
+FROM public.agent_transactions
+INNER JOIN public.agents ON agents.agent_id = agent_transactions.agent_id
+WHERE agent_transactions.when_created > now() -INTERVAL '7 days'
+GROUP BY city;
