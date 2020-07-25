8b6100bedcc5470790e881e17d4ce6a1aee7ebd9
djanie <kotey.seth11@gmail.com>
2020-07-25 13:53:36 +0000

    Update Wave_Schema_Queries.sql
    Update with comments, Q5, Q8 and Q9

--- Wave_Schema_Queries.sql
+++ Wave_Schema_Queries.sql
@@ -16,7 +16,16 @@ SELECT TO_CHAR(TO_DATE (EXTRACT(MONTH FROM when_created)::text, 'MM'), 'Month')	
 	GROUP BY EXTRACT(MONTH FROM agent_transactions.when_created);		--This groups the extracted number of agent transactions in 2018 by the month in which they were done
 
 --Q5
-
+WITH agent_withdrawers AS
+(SELECT COUNT (agent_id)
+AS net_withdrawers
+FROM agent_transactions 
+HAVING COUNT (amount)
+IN (SELECT COUNT (amount) FROM agent_transactions WHERE amount > -1 
+AND amount !=0 HAVING COUNT (amount) > (SELECT COUNT(amount)
+FROM agent_transactions WHERE amount < 1 AND  AMOUNT !=0)))
+SELECT net_withdrawers
+FROM agent_withdrawers;
 
 --Q6
 CREATE OR REPLACE VIEW atx_volume_city_summary AS 		--This creates a new table View which can be updated with columns without creating actual tables which will contain redundant data 
@@ -35,16 +44,31 @@ WHERE agent_transactions.when_created > now() -INTERVAL '7 days'		--This limits 
 GROUP BY city, country;													--This groups the results by the city and country in which they were performed
 
 --Q8
-
+CREATE OR REPLACE VIEW send_volume AS
+SELECT COUNT(atx_id) AS volume,kind,country
+FROM public.agent_transactions,public.transfers, public.agents
+WHERE agent_transactions.when_created>current_date -interval '7 days'
+GROUP BY kind,country;
 
 --Q9
-
+CREATE OR REPLACE VIEW send_volume AS
+SELECT COUNT(DISTINCT transfers.source_wallet_id) AS Unique_Senders,
+COUNT(transfer_id) AS Transaction_count,
+transfers.kind AS Transfer_Kind, wallets.ledger_location AS Country,
+SUM(transfers.send_amount_scalar) AS Volume
+FROM transfers INNER JOIN wallets
+ON transfers.source_wallet_id = wallets.wallet_id
+WHERE (transfers.when_created > (NOW() - INTERVAL '7 days'))
+GROUP BY wallets.ledger_location, transfers.kind;
 
 --Q10
-SELECT transfers.source_wallet_id, sum( transfers.send_amount_scalar) AS total_sent FROM transfers
-WHERE send_amount_currency = 'CFA'
-    AND (transfers.when_created > (now() - INTERVAL '10 month'))
-    GROUP BY transfers.source_wallet_id
-    HAVING sum( transfers.send_amount_scalar)>10000000
+SELECT transfers.source_wallet_id, sum( transfers.send_amount_scalar) AS total_sent FROM transfers	--This selects the source wallet id and sums up all values in the sent amount column
+WHERE send_amount_currency = 'CFA'		--This selects amounts which were sent in CFA
+    AND (transfers.when_created > (now() - INTERVAL '1 month'))	--This selects the transfers from the past month
+    GROUP BY transfers.source_wallet_id							--This groups the transfers by the wallet used to make the transfers
+    HAVING sum( transfers.send_amount_scalar)>10000000;			--This selects sums of transfers sent within the past month which have a value over 10,000,000 CFA
 	
+	
+	
+
 	
