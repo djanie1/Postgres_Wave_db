dc43985ebd98371dfbd862b98a6e3363f89fedd4
djanie <kotey.seth11@gmail.com>
2020-07-25 01:22:58 +0000

    Update Wave_Schema_Queries.sql
    Update with Q3 query

--- Wave_Schema_Queries.sql
+++ Wave_Schema_Queries.sql
@@ -5,3 +5,6 @@ SELECT COUNT(u_id) FROM public.users;
 SELECT COUNT(transfer_id) FROM public.transfers 
 	WHERE send_amount_currency = 'CFA';
 	
+--Q3
+SELECT DISTINCT COUNT(u_id) FROM public.transfers
+	WHERE send_amount_currency = 'CFA';
