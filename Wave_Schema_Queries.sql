8467b990e6d33bd9fa14825ece4e16188f53225a
djanie <kotey.seth11@gmail.com>
2020-07-25 02:05:32 +0000

    Update Wave_Schema_Queries.sql
    Update with Q4

--- Wave_Schema_Queries.sql
+++ Wave_Schema_Queries.sql
@@ -8,3 +8,9 @@ SELECT COUNT(transfer_id) FROM public.transfers
 --Q3
 SELECT DISTINCT COUNT(u_id) FROM public.transfers
 	WHERE send_amount_currency = 'CFA';
+
+--Q4
+SELECT TO_CHAR(TO_DATE (EXTRACT(MONTH FROM when_created)::text, 'MM'), 'Month')	AS months, 
+	COUNT(atx_id) FROM public.agent_transactions 
+	WHERE EXTRACT(YEAR FROM agent_transactions.when_created) = 2018 
+	GROUP BY EXTRACT(MONTH FROM agent_transactions.when_created);
